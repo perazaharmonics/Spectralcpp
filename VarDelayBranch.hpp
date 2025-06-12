745f6d90246036e4357b2bc8203cdb53e5fe76fa
J. Enrique Peraza, BScEE MEngEE <141517235+perazaharmonics@users.noreply.github.com>
2025-06-12 17:27:56 -0400

    Debugged

--- VarDelayBranch.hpp
+++ VarDelayBranch.hpp
@@ -16,15 +16,15 @@
 
 namespace dsp::wg
 {
-  template<size_t MaxLen = 1 << 15>
+  template<typename SampleT=float,size_t MaxLen=1<<15>
   struct VarDelayBranch final:public Node
   {
     VarDelayBranch(void) noexcept=default;
-    bool SetDelay(size_t d) noexcept { return (this->fracdl.SetDelay(d)); }
+    void SetDelay(size_t d) noexcept { return (this->fracdl.SetDelay(d)); }
     // Read a fractionally interpolated sample from the head.
     float Read(void) const noexcept { return this->fracdl.Read(); }
     // Look at head without advancing the delay line.
-    inline T Peek(void) const noexcept { return this->fracdl.PeekHead(); } // Read the sample at the head of the delay line without advancing it.
+    inline SampleT Peek(void) const noexcept { return this->fracdl.PeekHead(); } // Read the sample at the head of the delay line without advancing it.
     // Ramp the *total* delay (integer+fractional) to 'newDelay' over 'n' samples.
     void RampTo(float newDelay, size_t n) noexcept
     {                                   // ----------- RampTo ----------- //
@@ -40,7 +40,7 @@ namespace dsp::wg
         for (size_t i=0;i<n;++i)        // For each sample to propagate...
         {
           //this->fracdl.Write(this->fracdl.Read());// Propagate the sample across the line.
-          T v=fracdl.Read();           // Read the sample from the delay line.
+          SampleT v=fracdl.Read();           // Read the sample from the delay line.
           this->fracdl.Write(v);       // Write the sample back to the delay line.
           fracdl.Tick();               // Advance the delay line by one sample.
         }                              //
@@ -49,7 +49,7 @@ namespace dsp::wg
     inline size_t GetDelay(void) const noexcept { return this->fracdl.GetDelay(); }
     inline size_t GetMaxLen(void) const noexcept { return MaxLen; }
     private:
-      VarFracDelay<T,MaxLen> fracdl;// The delay line object.
+      VarFracDelay<SampleT,MaxLen> fracdl;// The delay line object.
       //static_assert(MaxLen > 0 && (MaxLen & (MaxLen - 1)) == 0, "MaxLen must be a power of two for efficient wrap-around.");
       //double len{MaxLen};             // The length of the delay line in samples.
    };
